5d1b283c3fd8ca2f9967216d324e59297ea2c1c8
basil lizotte <basil@btcl-advisory.com>
2021-05-23 18:42:30 +0200

    Enterpise ref

--- db/migrate/20210524142158_add_enterprise_id_to_user.rb
+++ db/migrate/20210524142158_add_enterprise_id_to_user.rb
@@ -1,5 +1,5 @@
 class AddEnterpriseIdToUser < ActiveRecord::Migration[6.0]
   def change
-    add_reference :users, :enterprise, null: false, foreign_key: true
+    add_reference :users, :enterprise, null: false, foreign_key: true 
   end
 end
