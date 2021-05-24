91c82027403343803d3268fd71b3e3df81c82bfd
maylis de la monneraye <maylisdlm@hotmail.fr>
2021-05-24 18:03:54 +0200

    controllercharitycampaign

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -10,17 +10,22 @@ class CampaignsController < ApplicationController
 
   def create
     @campaign = Campaign.new(campaign_params)
-    @campaign.save
-    redirect_to campaign_path(@campaign)
+    if @campaign.save
+      redirect_to campaign_path(@campaign)
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
-    @campaign.update(campaign_params)
-    redirect_to campaign_path(@campaign)
+    if @campaign.update(campaign_params)
+      redirect_to campaign_path(@campaign)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -28,15 +33,13 @@ class CampaignsController < ApplicationController
     redirect_to campaign_path(@campaign)
   end
 
-
   private
 
   def campaign_params
-    params.require(:campaign).permit(:step_convension, :max_contribution, :charity_event)
+    params.require(:campaign).permit(:step_convension, :max_contribution, :charity_event_id, :enterprise_id)
   end
 
   def find_campaign
     @campaign = Campaign.find(params[:id])
   end
-
 end
