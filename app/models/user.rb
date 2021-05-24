f5db683dc4465e9df951c12e411b68980551d85e
basil lizotte <basil@btcl-advisory.com>
2021-05-24 06:16:06 +0200

    user model repaired

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  belongs_to :enterprise, optional: true
+  has_many :teams, through: :joined_team
+  has_many :campaigns, through: :joined_campaign
+  has_one_attached :photo
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :enterprise
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
+  validates :nickname, presence: true, uniqueness: true
+
 end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_05_24_144711) do
+ActiveRecord::Schema.define(version: 2021_05_24_144712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +62,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_144711) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["campaign_id"], name: "index_enterprises_on_campaign_id"
   end
-  
+
   create_table "joined_campaigns", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.float "user_donation_event"
@@ -107,7 +106,12 @@ ActiveRecord::Schema.define(version: 2021_05_24_144711) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "enterprise_id", null: false
+    t.bigint "enterprise_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "address"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["enterprise_id"], name: "index_users_on_enterprise_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -116,10 +120,9 @@ ActiveRecord::Schema.define(version: 2021_05_24_144711) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "campaigns", "charity_events"
   add_foreign_key "enterprises", "campaigns"
-  add_foreign_key "steps", "users"
-  add_foreign_key "users", "enterprises"
   add_foreign_key "joined_campaigns", "users"
   add_foreign_key "joined_teams", "teams"
   add_foreign_key "joined_teams", "users"
   add_foreign_key "steps", "users"
+  add_foreign_key "users", "enterprises"
 end
