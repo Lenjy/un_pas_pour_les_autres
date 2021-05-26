7823401387ff45bd5d9d9fc4201ddcd99cf04de9
Benjamin Eycken <be.eycken@gmail.com>
2021-05-26 15:50:34 +0200

    user adapt with google

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_121946) do
+ActiveRecord::Schema.define(version: 2021_05_26_133223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 2021_05_25_121946) do
     t.string "phone_number"
     t.string "address"
     t.string "nickname"
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["enterprise_id"], name: "index_users_on_enterprise_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
