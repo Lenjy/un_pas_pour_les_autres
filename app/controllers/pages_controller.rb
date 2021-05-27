75793f21940740966f883efac6c604e14bdc5e0e
basil lizotte <basil@btcl-advisory.com>
2021-05-27 10:48:06 +0200

    I still have to do the design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,6 @@ class PagesController < ApplicationController
     @charity_event = CharityEvent.where("? BETWEEN date_beginning AND date_ending", Time.zone.now).last
     @step_count = current_user.steps.where("date BETWEEN ? AND ?", @charity_event.date_beginning, @charity_event.date_ending).sum(:nb_steps)  
     #define the status / ou filtre date
-
   end
 
 end
---
+++ app/javascript/components/donate_event.js
@@ -0,0 +1,20 @@
+
+
+const calculate = (conversion_proposal) => {
+    const steps = parseInt(document.querySelector('#total_steps').value);
+    const donation_output = document.querySelector('#donation_output');
+    let steps_calculation = steps / parseInt(conversion_proposal.value);
+    donation_output.value = steps_calculation;
+}
+
+const donate_event = () => {
+    const conversion_proposal = document.querySelector('#conversion_proposal');
+    conversion_proposal.addEventListener('keyup', (event) => {
+        calculate(conversion_proposal)
+
+    });    
+    /* const conversion_proposal.addEventListener('change', calculate); */
+}
+
+
+export {donate_event}
