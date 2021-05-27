a4616d8c4d784983ed8ea6fa598f6a24155564b9
Benjamin Eycken <be.eycken@gmail.com>
2021-05-27 14:41:43 +0200

    seed-test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -25,113 +25,113 @@ class PagesController < ApplicationController
 
   def secret
 
-    JoinedTeam.destroy_all
-    puts 'JT destroyed'
-    Step.destroy_all
-    puts 'steps destroyed'
-    # User.destroy_all
-    # puts 'users destroyed'
-    Team.destroy_all
-    puts 'teams destroyed'
-    Campaign.destroy_all
-    puts 'caamp destroyed'
-    Enterprise.destroy_all
-    puts 'enterp destroyed'
-    CharityEvent.destroy_all
-    puts 'ch ev destroyed'
+    # JoinedTeam.destroy_all
+    # puts 'JT destroyed'
+    # Step.destroy_all
+    # puts 'steps destroyed'
+    # # User.destroy_all
+    # # puts 'users destroyed'
+    # Team.destroy_all
+    # puts 'teams destroyed'
+    # Campaign.destroy_all
+    # puts 'caamp destroyed'
+    # Enterprise.destroy_all
+    # puts 'enterp destroyed'
+    # CharityEvent.destroy_all
+    # puts 'ch ev destroyed'
     
-    restos_du_coeur = CharityEvent.create!(
-    title: "Evenement Coluche 2021",
-    charity_name: "Les Restos du Coeur",
-    description: "Fondés par Coluche en 1985, les Restos du Coeur est une association loi de 1901, reconnue d’utilité publique, sous le nom officiel de Les Restaurants du Coeur – les Relais du Coeur.",
-    date_beginning: Date.new(2021, 5, 23),
-    date_ending: Date.new(2021, 6, 07),
-    total_donation: 0
-    )
+    # restos_du_coeur = CharityEvent.create!(
+    # title: "Evenement Coluche 2021",
+    # charity_name: "Les Restos du Coeur",
+    # description: "Fondés par Coluche en 1985, les Restos du Coeur est une association loi de 1901, reconnue d’utilité publique, sous le nom officiel de Les Restaurants du Coeur – les Relais du Coeur.",
+    # date_beginning: Date.new(2021, 5, 23),
+    # date_ending: Date.new(2021, 6, 07),
+    # total_donation: 0
+    # )
 
-    puts "Created #{CharityEvent.all.size} charity events in seeds.rb"
+    # puts "Created #{CharityEvent.all.size} charity events in seeds.rb"
 
-    # ENTERPRISES
+    # # ENTERPRISES
 
-    le_wagon = Enterprise.create!(name: "Le Wagon")
+    # le_wagon = Enterprise.create!(name: "Le Wagon")
 
 
-    puts "Created #{Enterprise.all.size} enterprises in seeds.rb"
+    # puts "Created #{Enterprise.all.size} enterprises in seeds.rb"
 
-    # CAMPAIGNS
+    # # CAMPAIGNS
 
-    campaign_le_wagon = Campaign.create!(
-      step_conversion: 0,
-      max_contribution: 100000,
-      charity_event: restos_du_coeur,
-      enterprise: le_wagon
-    )
+    # campaign_le_wagon = Campaign.create!(
+    #   step_conversion: 0,
+    #   max_contribution: 100000,
+    #   charity_event: restos_du_coeur,
+    #   enterprise: le_wagon
+    # )
 
-    puts "Created #{Campaign.all.size} campaigns in seeds.rb"
+    # puts "Created #{Campaign.all.size} campaigns in seeds.rb"
 
-    # USERS
-    basil = User.all.where(email: "basil.lizotte@gmail.com").first
-    arthur = User.all.where(email: "maguinarthur@gmail.com").first
-    benjamin = User.all.where(email: "be.eycken@gmail.com").first
-    maylis = User.all.where(email: "mdlmaylis@gmail.com").first
+    # # USERS
+    # basil = User.all.where(email: "basil.lizotte@gmail.com").first
+    # arthur = User.all.where(email: "maguinarthur@gmail.com").first
+    # benjamin = User.all.where(email: "be.eycken@gmail.com").first
+    # maylis = User.all.where(email: "mdlmaylis@gmail.com").first
 
-    # basil = User.create!(first_name: "Basil", last_name: "Lizotte", nickname: "lizottebasil", address: "4 rue Victor Masse", phone_number: "0675674509", email: "basil.lizotte@gmail.com", password: "123456", password_confirmation: "123456")
+    # # basil = User.create!(first_name: "Basil", last_name: "Lizotte", nickname: "lizottebasil", address: "4 rue Victor Masse", phone_number: "0675674509", email: "basil.lizotte@gmail.com", password: "123456", password_confirmation: "123456")
 
-    # arthur = User.create!(first_name: "Arthur", last_name: "Maguin", nickname: "maguinarthur", address: "20 rue Victor Masse", phone_number: "0675674512", email: "maguinarthur@gmail.com", password: "123456", password_confirmation: "123456")
+    # # arthur = User.create!(first_name: "Arthur", last_name: "Maguin", nickname: "maguinarthur", address: "20 rue Victor Masse", phone_number: "0675674512", email: "maguinarthur@gmail.com", password: "123456", password_confirmation: "123456")
 
-    # benjamin = User.create!(first_name: "Benjamin", last_name: "Eycken", nickname: "eyckenbenjamin", address: "20 rue Victor Masse", phone_number: "0675674512", email: "be.eycken@gmail.com", password: "123456", password_confirmation: "123456")
+    # # benjamin = User.create!(first_name: "Benjamin", last_name: "Eycken", nickname: "eyckenbenjamin", address: "20 rue Victor Masse", phone_number: "0675674512", email: "be.eycken@gmail.com", password: "123456", password_confirmation: "123456")
 
-    # maylis = User.create!(first_name: "Maylis", last_name: "De La Monneraye", nickname: "dlmmaylis", address: "20 rue Victor Masse", phone_number: "0675674512", email: "mdlmaylis@gmail.com", password: "123456", password_confirmation: "123456")
+    # # maylis = User.create!(first_name: "Maylis", last_name: "De La Monneraye", nickname: "dlmmaylis", address: "20 rue Victor Masse", phone_number: "0675674512", email: "mdlmaylis@gmail.com", password: "123456", password_confirmation: "123456")
 
-    photo_basil = URI.open('./app/assets/images/basil.jpeg')
-    basil.photo.attach(io: photo_basil, filename: 'basil_demo.jpg', content_type: 'image/jpg')
+    # photo_basil = URI.open('./app/assets/images/basil.jpeg')
+    # basil.photo.attach(io: photo_basil, filename: 'basil_demo.jpg', content_type: 'image/jpg')
 
-    photo_arthur = URI.open('./app/assets/images/arthur.jpeg')
-    arthur.photo.attach(io: photo_arthur, filename: 'arthur_demo.jpg', content_type: 'image/jpg')
+    # photo_arthur = URI.open('./app/assets/images/arthur.jpeg')
+    # arthur.photo.attach(io: photo_arthur, filename: 'arthur_demo.jpg', content_type: 'image/jpg')
 
-    photo_benjamin = URI.open('./app/assets/images/benjamin.jpeg')
-    benjamin.photo.attach(io: photo_benjamin, filename: 'benjamin_demo.jpg', content_type: 'image/jpg')
+    # photo_benjamin = URI.open('./app/assets/images/benjamin.jpeg')
+    # benjamin.photo.attach(io: photo_benjamin, filename: 'benjamin_demo.jpg', content_type: 'image/jpg')
 
-    photo_maylis = URI.open('./app/assets/images/maylis.webp')
-    maylis.photo.attach(io: photo_maylis, filename: 'maylis_demo.jpg', content_type: 'image/jpg')
+    # photo_maylis = URI.open('./app/assets/images/maylis.webp')
+    # maylis.photo.attach(io: photo_maylis, filename: 'maylis_demo.jpg', content_type: 'image/jpg')
 
     
-    puts "Created #{User.all.size} users in seeds.rb"
+    # puts "Created #{User.all.size} users in seeds.rb"
 
 
 
-    # STEPS
+    # # STEPS
 
-    def is_integer(number)
-      number.floor == number
-    end
+    # def is_integer(number)
+    #   number.floor == number
+    # end
 
 
-    (3..30).each do |i|
+    # (3..30).each do |i|
 
-      Step.create!(
-      date: Date.parse("2021-05-#{i}") ,
-      nb_steps: rand(1000..15000),
-      week: (is_integer((i-3)/7) ? (i-3)/7 + 1 : ((i-3)/7).floor + 1),
-      user_id: basil.id)
+    #   Step.create!(
+    #   date: Date.parse("2021-05-#{i}") ,
+    #   nb_steps: rand(1000..15000),
+    #   week: (is_integer((i-3)/7) ? (i-3)/7 + 1 : ((i-3)/7).floor + 1),
+    #   user_id: basil.id)
 
-      Step.create!(
-      date: Date.parse("2021-05-#{i}") ,
-      nb_steps: rand(1000..15000),
-      week: (is_integer((i-3)/7) ? (i-3)/7 + 1 : ((i-3)/7).floor + 1),
-      user_id: arthur.id)
+    #   Step.create!(
+    #   date: Date.parse("2021-05-#{i}") ,
+    #   nb_steps: rand(1000..15000),
+    #   week: (is_integer((i-3)/7) ? (i-3)/7 + 1 : ((i-3)/7).floor + 1),
+    #   user_id: arthur.id)
 
-      Step.create!(
-      date: Date.parse("2021-05-#{i}") ,
-      nb_steps: rand(1000..15000),
-      week: (is_integer((i-3)/7) ? (i-3)/7 + 1 : ((i-3)/7).floor + 1),
-      user_id: benjamin.id)
+    #   Step.create!(
+    #   date: Date.parse("2021-05-#{i}") ,
+    #   nb_steps: rand(1000..15000),
+    #   week: (is_integer((i-3)/7) ? (i-3)/7 + 1 : ((i-3)/7).floor + 1),
+    #   user_id: benjamin.id)
 
-      Step.create!(
-      date: Date.parse("2021-05-#{i}") ,
-      nb_steps: rand(1000..15000),
-      week: (is_integer((i-3)/7) ? (i-3)/7 + 1 : ((i-3)/7).floor + 1),
-      user_id: maylis.id)
+    #   Step.create!(
+    #   date: Date.parse("2021-05-#{i}") ,
+    #   nb_steps: rand(1000..15000),
+    #   week: (is_integer((i-3)/7) ? (i-3)/7 + 1 : ((i-3)/7).floor + 1),
+    #   user_id: maylis.id)
 
     end
 
