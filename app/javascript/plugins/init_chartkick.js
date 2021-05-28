e90053846d3e50ac50994fb0e726c31385561248
Arthur Maguin <maguinarthur@gmail.com>
2021-05-28 14:34:38 +0200

    ok

--- app/javascript/plugins/init_chartkick.js
+++ app/javascript/plugins/init_chartkick.js
@@ -2,47 +2,26 @@ import Chartkick from "chartkick";
 
 const initChartkickWeek = () => {
   const div_week = document.getElementById('chart-week');
-<<<<<<< colors
-  console.log(div_week);
-  const data_week = JSON.parse(div_week.dataset.steps);
-  console.log(data_week);
-  new Chartkick.ColumnChart("chart-week", data_week, { colors: ["rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 1)"]});
-=======
-
-  if (div_week){
-    console.log(div_week);
+  if (div_week) {
     const data_week = JSON.parse(div_week.dataset.steps);
-    console.log(data_week);
-    new Chartkick.ColumnChart("chart-week", data_week);
-  }
->>>>>>> master
-}
+    new Chartkick.ColumnChart("chart-week", data_week, { colors: ["rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 0.5)", "rgba(109, 168, 179, 1)"]});
+  };}
 
 const initChartkickMonth = () => {
   const div_month = document.getElementById('chart-month');
-<<<<<<< colors
-  const data_month = JSON.parse(div_month.dataset.steps);
-  new Chartkick.LineChart("chart-month", data_month, { curve: true, colors: ["rgb(109, 168, 179)"], label: "Nombre de pas" });
-=======
-  if (div_month){
+  if (div_month) {
     const data_month = JSON.parse(div_month.dataset.steps);
-    new Chartkick.LineChart("chart-month", data_month, { curve: true });
-  }
->>>>>>> master
-}
+    new Chartkick.LineChart("chart-month", data_month, { curve: true, colors: ["rgb(109, 168, 179)"], label: "Nombre de pas" });
+  };}
+
 
 const initChartkickTeamOne = () => {
   const div_team_one = document.getElementById('chart-team-one');
-<<<<<<< colors
-  const data_team_one = JSON.parse(div_team_one.dataset.steps);
-  new Chartkick.ColumnChart("chart-team-one", data_team_one, { colors: ["rgb(109, 168, 179)"], label: "Nombre de pas"  });
-=======
   if (div_team_one) {
     const data_team_one = JSON.parse(div_team_one.dataset.steps);
-    new Chartkick.ColumnChart("chart-team-one", data_team_one);
-  }
->>>>>>> master
-}
+    new Chartkick.ColumnChart("chart-team-one", data_team_one, { colors: ["rgb(109, 168, 179)"], label: "Nombre de pas"  });
+  };}
+
 
 export { initChartkickWeek };
 export { initChartkickMonth };
