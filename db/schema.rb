9766910a2fd2716b604f103d977441372b6730df
Arthur Maguin <maguinarthur@gmail.com>
2021-06-02 09:28:41 +0200

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -36,18 +36,6 @@ ActiveRecord::Schema.define(version: 2021_05_31_143529) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "campaigns", force: :cascade do |t|
     t.integer "step_conversion"
     t.integer "max_contribution"
